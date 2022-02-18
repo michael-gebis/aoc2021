74930eca896bf1972795a8b19d8dcc5a9ec4f433
Michael Gebis <mike.gebis@gmail.com>
2022-02-17 17:20:49 -0800

    Day18: magnitude works
    I have parameterized on T, but it's pretty pointless and makes
    a lot of the code harder to read.  So I may undo all that.

--- src/day18/mod.rs
+++ src/day18/mod.rs
@@ -39,6 +39,46 @@ mod tests {
         println!("Node {:?}", z);
         assert_eq!(z.magnitude(), 29);
     }
+
+    #[test]
+    fn test_parse_snailfish() {
+        let q = parse_snailfish::<u64>("[9,1]");
+        println!("calculating magnitude of {:?}", q);
+        assert_eq!(q.magnitude(), 29);
+
+        let q = parse_snailfish::<u64>("[1,9]");
+        println!("calculating magnitude of {:?}", q);
+        assert_eq!(q.magnitude(), 21);
+
+        let q = parse_snailfish::<u64>("[[9,1],[1,9]]");
+        println!("calculating magnitude of {:?}", q);
+        assert_eq!(q.magnitude(), 129);
+
+        let q = parse_snailfish::<u64>("[[1,2],[[3,4],5]]");
+        println!("calculating magnitude of {:?}", q);
+        assert_eq!(q.magnitude(), 143);
+
+        let q = parse_snailfish::<u64>("[[[[0,7],4],[[7,8],[6,0]]],[8,1]]");
+        println!("calculating magnitude of {:?}", q);
+        assert_eq!(q.magnitude(), 1384);
+        
+        let q = parse_snailfish::<u64>("[[[[1,1],[2,2]],[3,3]],[4,4]]");
+        println!("calculating magnitude of {:?}", q);
+        assert_eq!(q.magnitude(), 445);
+
+        let q = parse_snailfish::<u64>("[[[[3,0],[5,3]],[4,4]],[5,5]]");
+        println!("calculating magnitude of {:?}", q);
+        assert_eq!(dbg!(q.magnitude()), 791);
+
+        let q = parse_snailfish::<u64>("[[[[5,0],[7,4]],[5,5]],[6,6]]");
+        println!("calculating magnitude of {:?}", q);
+        assert_eq!(q.magnitude(), 1137);
+
+        let q = parse_snailfish::<u64>("[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]");
+        println!("calculating magnitude of {:?}", q);
+        assert_eq!(dbg!(q.magnitude()), 3488);
+
+    }
 }
 
 // I don't want to write a full lexer/parser... but maybe I have to.
@@ -51,7 +91,51 @@ fn parse_snailfish<T: num::Integer + num::NumCast>(x: &str) -> BinTreeNode<T> {
     let re_pair = Regex::new(r"^\[(.+),(.+)\]$").unwrap();
     let re_digits = Regex::new(r"^(\d+)$").unwrap();
 
-    BinTreeNode::Leaf(num::NumCast::from(77).unwrap())
+    let mut pos:usize = 0;
+    let c:Vec<char> = x.chars().collect();
+    parse_snailfish_pos::<T>(&c, &mut pos)
+
+    //BinTreeNode::Leaf(num::NumCast::from(77).unwrap())
+}
+
+fn parse_snailfish_pos<T: num::Integer + num::NumCast>(x: &[char], pos: &mut usize) -> BinTreeNode<T> {
+
+    //let mut chars x[*pos..].chars();
+
+    //let n = chars.next();
+    //let n = x.get();
+    
+    match x[*pos] {
+        '[' => {
+            println!("open bracket");
+            *pos +=1;
+            let left:BinTreeNode<T> = parse_snailfish_pos(x,pos);
+            *pos+=1;
+            let right:BinTreeNode<T> = parse_snailfish_pos(x,pos);
+            *pos+=1;
+            return BinTreeNode::Branch{ left:Box::new(left), right:Box::new(right) };
+        },
+        _ => {
+            println!("number..."); 
+            let mut val:i64 = 0;
+
+            if x[*pos].is_digit(10) {
+                while x[*pos].is_digit(10) {
+                    val *= 10;
+                    val += x[*pos].to_digit(10).unwrap() as i64;
+                    *pos += 1;
+                }
+                println!("Found number {}", val);
+            } else {
+                panic!("wtf!");
+            }
+            return BinTreeNode::Leaf(num::NumCast::from(val).unwrap());
+
+        }
+    }
+
+    //println!("What oh...");
+    //BinTreeNode::Leaf(num::NumCast::from(77).unwrap())
 }
 
 // Some binary tree links:
