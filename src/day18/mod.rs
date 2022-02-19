e0b8b191ddf86bc2b4adda9f87405541e403f85f
Michael Gebis <mike.gebis@gmail.com>
2022-02-18 16:41:13 -0800

    Day18, fmt::Display and more testing.
    Next step: implement parent pointers so I can find "previous" and
    "next" nodes, needed by "explode".

--- src/day18/mod.rs
+++ src/day18/mod.rs
@@ -2,6 +2,7 @@ use crate::*;
 //use num::{Integer, NumCast};
 //use regex::Regex;
 //use std::collections::HashMap;
+use std::fmt;
 
 const FILENAME: &str = "src/day18/day18_input.txt";
 //const FILENAME: &str = "src/day18/day18_example.txt";
@@ -11,6 +12,7 @@ const FILENAME: &str = "src/day18/day18_input.txt";
 // it to learn, to BinTree it is.
 #[allow(dead_code)]
 #[derive(Debug)]
+#[derive(PartialEq)]
 enum BinTreeNode {
     Leaf(i64),
     Branch {
@@ -25,6 +27,35 @@ impl BinTreeNode {
         match self {
             Self::Leaf(t) => t,
             Self::Branch { left, right } => left.magnitude() * 3 + right.magnitude() * 2,
+        }
+    }
+
+    fn recursive_split(n:&mut BinTreeNode) -> bool {
+        match n {
+            BinTreeNode::Leaf(t) => {
+                if *t >= 10 {
+                    let lval = (*t+1)/2; // round up
+                    let rval = *t/2;     // round down
+                    *n = BinTreeNode::Branch {
+                        left: Box::new(BinTreeNode::Leaf(lval)),
+                        right: Box::new(BinTreeNode::Leaf(rval)),
+                    };
+                    return true;
+                }
+                false
+            }
+            Self::Branch { left, right } => BinTreeNode::recursive_split(left) || BinTreeNode::recursive_split(right)
+        }
+    }
+}
+
+// I just think recursion is prety neat.
+#[allow(dead_code)]
+impl fmt::Display for BinTreeNode {
+    fn fmt(&self, f: &mut fmt::Formatter) -> std::result::Result<(), std::fmt::Error> {
+        match self {
+            Self::Leaf(t) => write!(f, "{}", t),
+            Self::Branch {left, right} => write!(f, "[{},{}]", left, right),
         }
     }
 }
@@ -48,39 +79,61 @@ mod tests {
     #[test]
     fn test_parse_snailfish() {
         let q = parse_snailfish("[9,1]");
-        println!("calculating magnitude of {:?}", q);
+        println!("calculating magnitude of {}", q);
         assert_eq!(q.magnitude(), 29);
 
         let q = parse_snailfish("[1,9]");
-        println!("calculating magnitude of {:?}", q);
+        println!("calculating magnitude of {}", q);
         assert_eq!(q.magnitude(), 21);
 
         let q = parse_snailfish("[[9,1],[1,9]]");
-        println!("calculating magnitude of {:?}", q);
+        println!("calculating magnitude of {}", q);
         assert_eq!(q.magnitude(), 129);
 
         let q = parse_snailfish("[[1,2],[[3,4],5]]");
-        println!("calculating magnitude of {:?}", q);
+        println!("calculating magnitude of {}", q);
         assert_eq!(q.magnitude(), 143);
 
         let q = parse_snailfish("[[[[0,7],4],[[7,8],[6,0]]],[8,1]]");
-        println!("calculating magnitude of {:?}", q);
+        println!("calculating magnitude of {}", q);
         assert_eq!(q.magnitude(), 1384);
+
         let q = parse_snailfish("[[[[1,1],[2,2]],[3,3]],[4,4]]");
-        println!("calculating magnitude of {:?}", q);
+        println!("calculating magnitude of {}", q);
         assert_eq!(q.magnitude(), 445);
 
         let q = parse_snailfish("[[[[3,0],[5,3]],[4,4]],[5,5]]");
-        println!("calculating magnitude of {:?}", q);
+        println!("calculating magnitude of {}", q);
         assert_eq!(dbg!(q.magnitude()), 791);
 
         let q = parse_snailfish("[[[[5,0],[7,4]],[5,5]],[6,6]]");
-        println!("calculating magnitude of {:?}", q);
+        println!("calculating magnitude of {}", q);
         assert_eq!(q.magnitude(), 1137);
 
         let q = parse_snailfish("[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]");
-        println!("calculating magnitude of {:?}", q);
+        println!("calculating magnitude of {}", q);
         assert_eq!(dbg!(q.magnitude()), 3488);
+    }
+
+    #[test]
+    fn test_recursive_split() {
+        let q = &mut parse_snailfish("[10,1]");
+        BinTreeNode::recursive_split(q);
+        let expected = parse_snailfish("[[5,5],1]");
+        println!("split={}",q);
+        assert_eq!(expected,*q);
+
+        let q = &mut parse_snailfish("[1,11]");
+        BinTreeNode::recursive_split(q);
+        let expected = parse_snailfish("[1,[6,5]]");
+        println!("split={}",q);
+        assert_eq!(expected,*q);
+
+        let q = &mut parse_snailfish("[12,15]");
+        while BinTreeNode::recursive_split(q) {
+            println!("doing split...");
+        }
+        println!("split={}",q);
     }
 }
 
