c1b5dceec7363ff86fac5e552d6a0081e070d0c3
Michael Gebis <mike.gebis@gmail.com>
2022-03-06 21:27:02 -0800

    Day23 notes

--- src/day23/mod.rs
+++ src/day23/mod.rs
@@ -5,8 +5,33 @@ use std::collections::HashSet;
 use std::collections::VecDeque;
 use std::fmt;
 
-//const FILENAME: &str = "src/day23/day23_example.txt";
-const FILENAME: &str = "src/day23/day23_input.txt";
+const FILENAME: &str = "src/day23/day23_example.txt";
+//const FILENAME: &str = "src/day23/day23_input.txt";
+
+// General strategery:
+// Create a list of "boards yet to be evaluated"
+// For the first board, look at each piece
+// For each piece, generate legal moves
+// Put that new board on the list of "boards yet to be evaluate"
+// Also: Keep a cache of already evaluated boards
+// Also: Keep a "best score so far" number; don't evaluate boards that are greater than that number
+
+/* 
+Empty board
+#############
+#...........#
+###.#.#.#.###
+  #.#.#.#.#
+  #########
+
+Note that pieces cannot stop in several positions, as shown by the X:
+#############
+#..X.X.X.X..#
+###.#.#.#.###
+  #.#.#.#.#
+  #########
+
+*/
 
 #[allow(dead_code)]
 pub fn day23_p1() {
